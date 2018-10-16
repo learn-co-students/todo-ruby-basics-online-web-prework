c32b9dde77aa1748a922cc7e17294873681adb98
Corina Talavera <corina.talavera@gmail.com>
2018-10-16 13:24:44 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,63 @@
-def division(num1, num2)
- 
-end
+describe "ruby" do
 
-def assign_variable(value)
-  
-end
+  describe "#division" do
+    division(num1,num2)
+      num1 = 42
+      num2 = 7
+expect(division(num1,num2)).to eq(6)
+    end
+  end
 
-def argue
-  
-end
+  describe '#assign_variable' do
+      value = "Bob"
+      puts value
 
-def greeting
+    end
+  end
 
-end
+  describe '#argue' do
+    it 'accepts an argument and return that argument as is' do
+      phrase = "I'm right and you are wrong!"
 
-def return_a_value
+      expect(argue(phrase)).to eq(phrase)
+    end
+  end
 
-end
+  describe '#greeting' do
+    it 'takes two arguments' do
+      greeting = "Hi there, "
+      name = "Bobby!"
+      puts (greeting,name)
 
-def last_evaluated_value
+      expect{ greeting(greeting, name) }.to_not raise_error
+    end
 
-end
+    it 'raises an error with one argument' do
+      greeting = "Hi there, "
 
-def pizza_party
+      expect{ greeting(greeting)}.to raise_error
+    end
+  end
 
+  describe '#return_a_value' do
+    it 'returns the phrase "Nice"' do
+      expect(return_a_value).to eq("Nice")
+    end
+  end
+
+  describe '#last_evaluated_value' do
+    it 'returns the phrase "expert"' do
+      expect(last_evaluated_value).to eq("expert")
+    end
+  end
+
+  describe '#pizza_party' do
+    it 'returns "cheese" by default' do
+      expect(pizza_party).to eq("cheese")
+    end
+
+    it 'returns the argument it received' do
+      expect(pizza_party("pepperoni")).to eq("pepperoni")
+    end
+  end
 end
