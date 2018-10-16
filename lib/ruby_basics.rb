1b3ac93efc8d8075d99351494d2843dfe82ec03c
Dakota Clark <bannaray95@gmail.com>
2018-10-16 00:01:36 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -2,26 +2,29 @@ def division(42/7)
 
 end
 
-def assign_variable(value)
-  
+def assign_variable
+  "Bob"
 end
 
 def argue
-  
+  phrase = "I'm right and you are wrong!"
+  puts phrase
 end
 
 def greeting
-
+greeting = "Hi there," #(name)
+name = "Bobby!"
+puts greeting, name
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
 def pizza_party
-
+puts "cheese"
 end
