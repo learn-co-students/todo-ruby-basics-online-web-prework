304201fc510bec0afb4f0ed00779e90b4240f2a4
biancacharlotin <biancacharlotin@gmail.com>
2018-10-29 19:45:49 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1=12 / num2=2 
 end
 
 def assign_variable(value)
+    value="Bob"
+end
+
+def argue(phrase)
+  puts phrase
+  return phrase
+
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting( greetings,  name )
 
 end
 
 def return_a_value
-
+   return return_a_value ="Nice"
 end
 
 def last_evaluated_value
-
+ return last_evaluated_value="expert"
 end
 
-def pizza_party
+def pizza_party(type="cheese")
+  puts type
+  return type
 
 end
