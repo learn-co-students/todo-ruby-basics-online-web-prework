227153229f0d6c6485fe922f56cd81240940e4b1
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-20 02:33:19 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  giraffe = value 
 end
 
-def argue
-  
+def argue(chunk)
+  chunk
 end
 
-def greeting
-
+def greeting(name, height)
+  puts "Hi, my name is #{name} and I am #{height}."
 end
 
-def return_a_value
-
+def return_a_value(value = "Nice")
+  value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+ phrase
 end
 
-def pizza_party
-
+def pizza_party(choice = "cheese")
+  choice
 end
