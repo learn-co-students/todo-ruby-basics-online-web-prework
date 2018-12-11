cfb72bbc2a646455e67154ffad4d1f52732353eb
Luis Zabala <luis.zabala076@gmail.com>
2018-12-11 00:33:20 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+return num1/num2
 end
 
 def assign_variable(value)
+  greg=value
+end
+
+def argue (argument)
+  return argument
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(hello, goodbye)
 
 end
 
-def return_a_value
-
+def return_a_value(phrase="Nice")
+return phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase="expert")
+return phrase
 end
 
-def pizza_party
-
+def pizza_party(pizza='cheese')
+  return pizza
 end
