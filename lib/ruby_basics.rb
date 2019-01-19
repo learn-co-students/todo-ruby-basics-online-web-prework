884ca8cfcf951802c5142921a207cef2d38ac2a3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-19 12:28:34 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  result = num1 / num2
+  result
 end
 
 def assign_variable(value)
-  
+  value_var = value
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
+def pizza_party(ingredients = "cheese")
+  return ingredients
 
 end
