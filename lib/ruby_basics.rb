e692c9710fb999c7c1fd415cdd05236c4f94a72b
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-21 05:15:57 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  
 end
 
 def assign_variable(value)
-  
+  puts "Bob"
 end
 
 def argue
-  
+  "argue"
 end
 
-def greeting
+def greeting (greeting, name)
 
 end
 
 def return_a_value
-
+  puts "nice"
 end
 
-def last_evaluated_value
+def last_evaluated_value("cheese")
 
 end
 
 def pizza_party
-
+puts pizza_party
 end
