ced90b5f65c16bd89081e933bff11bbe3d77ea52
lieby weintraub <liebahinda@gmail.com>
2019-01-23 08:28:14 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,20 +10,19 @@ def argue(phrase)
 phrase= "I'm right and you are wrong!"
 end
 
-def greeting(name)
+def greeting
   
-  
-
 end
 
 def return_a_value
+  return_a_value="Nice"
 
 end
 
 def last_evaluated_value
-
+  last_evaluated_value="expert"
 end
 
 def pizza_party
-
+  pizza_party="cheese"
 end
