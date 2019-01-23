63ad7b85662fc1b37db2b9bc9c0cd7b817f04548
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-23 18:27:31 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,43 @@
 def division(num1, num2)
- 
+ result=num1/num2
+ result
 end
+
+division(12, 2)
 
 def assign_variable(value)
-  
+  person=value
 end
 
-def argue
-  
+assign_variable("Bob")
+
+def argue(value="whatever")
+  return value
 end
 
-def greeting
+argue()
 
+def greeting(greeting, name)
+  puts "#(greeting), #(name)"
 end
+
+greeting("Hello", "Bob")
 
 def return_a_value
-
+  return "Nice"
 end
+
+return_a_value
 
 def last_evaluated_value
-
+  x=1+1
+  joey="expert"
 end
 
-def pizza_party
+return_a_value
 
+def pizza_party(topping="cheese")
+  topping
 end
+
+pizza_party
