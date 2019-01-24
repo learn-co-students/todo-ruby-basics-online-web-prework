2a8603c5c4048594ee2d7ddb7f22580d9b9ead7c
Kelly Sciandra <kellysciandra@icloud.com>
2019-01-24 16:20:35 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
-def division(num1, num2)
- 
+def division (num1 = 42, num2=7)
+  (num1/num2)
 end
 
-def assign_variable(value)
-  
+def assign_variable(name = "Bob")
+  name
 end
 
-def argue
-  
+def argue(name)
+"#{name}"
 end
 
-def greeting
-
+def greeting(name, language)
+  puts "#{name}, #{language}"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(flavor = "cheese")
+  "#{flavor}"
 end
+
+pizza_party("pepperoni")
