a804bba6961362f9ba97ecc8ce37bce46350cc99
Jay Sokolowski <jaysoko@gmail.com>
2019-01-29 03:00:45 +0000

    Ruby Basics updates

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num=num1/num2
+ num
 end
 
-def assign_variable(value)
+def assign_variable(value="Bob")
+  "#{value}"
+end
+
+def argue(name)
+  "#{name}"
+end
+
+def greeting(greeting,name)
   
 end
 
-def argue
-  
+def return_a_value(phrase="Nice")
+  "#{phrase}"
 end
 
-def greeting
-
+def last_evaluated_value(phrase="expert")
+  "#{phrase}"
 end
 
-def return_a_value
-
-end
-
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party(topping="cheese")
+  "#{topping}"
 end
