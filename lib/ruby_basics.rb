395c7638e3471004c2742058ae80b9c0dea34193
Samuel W Tong <ssamt1142@gmail.com>
2019-01-31 01:30:13 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  puts "#{value}"
+  "#{value}"
 end
 
-def argue
-  
+def argue(ment)
+  "#{ment}"
 end
 
-def greeting
+def greeting(ment1, ment2)
 
 end
 
 def return_a_value
-
+"Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  "#{topping}"
 end
