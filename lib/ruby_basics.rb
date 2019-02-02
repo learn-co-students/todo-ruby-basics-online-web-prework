b014df92821cd40ab8357b76b3a7943de1ce4362
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-02 18:59:03 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  puts 12 / 2 = 6
 end
 
-def assign_variable(value)
-  
+def assign_variable("Bob")
+  puts "Bob"
 end
 
-def argue
-  
+def argue(num)
+  puts 13
 end
 
-def greeting
-
+def greeting(name, language = "Ruby Programmer")
+ puts "Hello #{name}, the #{language}!"
 end
 
-def return_a_value
-
+def return_a_value(name)
+  puts "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value("expert")
+  puts "expert"
 end
 
-def pizza_party
-
+def pizza_party(name)
+  puts "cheese"
 end
