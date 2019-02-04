0944c786f2811ff1ae9d45d9bf5fec6b70031056
Jacqi E <jackie.entz@gmail.com>
2019-02-04 20:35:58 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
-def division(num1, num2)
- 
+require 'pry'
+def division(num1=42, num2=7)
+ num1 / num2
+end
+def assign_variable(value= "Bob")
+  value
 end
 
-def assign_variable(value)
-  
+def argue (phrase= "I'm right and you are wrong!")
+  phrase
 end
 
-def argue
-  
+def greeting(greeting="Hi there, ", name="Bobby!")
+  puts #{greeting} #{name}
 end
 
-def greeting
+#def greeting (greet="Hi there, ", name="Bobby!")
+#   puts greet name
+#binding.pry
+#end
 
-end
-
-def return_a_value
-
+def return_a_value 
+  it = "Nice"
 end
 
 def last_evaluated_value
-
+  it = "expert" 
 end
 
-def pizza_party
-
+def pizza_party (it="cheese")
+ it  
 end
