330cf3d8a4a4845cbd28221322175a5a65a9a8dc
Mark Crowder <crowdered@gmail.com>
2019-02-14 14:58:08 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,41 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+  num1 = 42, num2 = 7
+  sum = "num1" / "num2"
+  puts sum
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  name = "Bob"
 end
 
 def argue
-  
+  phrase = "I'm right and you are wrong!"
+  puts 
 end
 
-def greeting
-
+def greeting(greeting,name)
+  puts "#{greeting} #{name}!"
+  greeting("Hi there", "Bobby")
 end
+
+def greeting(greeting,name)
+  puts "#{greeting},"
+  end
 
 def return_a_value
-
+  mood = "Nice"
 end
 
 def last_evaluated_value
-
+  name = "Mark"
+  skill_level = "expert"
 end
 
 def pizza_party
-
+  size = "large"
+  topping = "cheese"
 end
+
+def pizza_party
+  second_topping = "pepperoni"
+  puts second_topping
