dfe6d27c9380f2c7aa523ff58d8d221c205ad404
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-18 01:40:15 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  return (value)
 end
 
-def argue
-  
+def argue (persons_name)
+  return (persons_name)
 end
 
-def greeting
+def greeting(name, phrase)
 
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
-
+def pizza_party (phrase = "cheese")
+puts "#{phrase}"
+return "#{phrase}"
 end
