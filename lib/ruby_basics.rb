cc4e82b3de868bc41148864cbc14429182e8b7ff
chandra mohan <mohaniyer_78@hotmail.com>
2019-02-21 07:39:07 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -2,26 +2,33 @@ def division(num1, num2)
  
 end
 
-def assign_variable(value)
+def assign_variable("mohan")
+  
+name="mohan"
   
 end
 
-def argue
+def argue(name)
+  
+  return name
   
 end
 
-def greeting
+def greeting(name, place)
+  puts "Hi #{name}, we heard you hail from this #{place}"
 
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
-
+def pizza_party(name ="cheese")
+  
+return name
 end
