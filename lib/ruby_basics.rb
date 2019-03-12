af47a2d8f4caaa08c669ad4fb525554fa461e6f4
Michael Chen <michaelchengaming@gmail.com>
2019-03-12 22:44:36 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ answer = num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(yoyoyo)
+  return yoyoyo
+
 end
 
-def greeting
+def greeting(x, y)
 
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
+  return topping
 
 end
