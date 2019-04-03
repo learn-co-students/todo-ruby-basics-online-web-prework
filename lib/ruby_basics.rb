7a4b837eb6c79deb3b45bf2185ef4881b80cf400
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-03 23:13:11 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  quotient = num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  student = name
 end
 
-def argue
-  
+def argue(string)
+  string
 end
 
-def greeting
-
+def greeting(name, age)
+  puts "Hi! My name is #{name} an I'm #{age} years old."
 end
 
 def return_a_value
-
+  phrase = "Nice"
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
