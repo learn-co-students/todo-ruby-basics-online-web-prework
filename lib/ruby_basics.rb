bba25e16a22e4d629f69ccad341cf12ea0643eec
Eric Bodden <ericbodden30@gmail.com>
2019-04-03 21:40:48 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1 = "42"
+ num2 = "7"
 end
 
 def assign_variable(value)
+  value = "Maeve"
   
 end
 
 def argue
+  phrase= "I'm right and you're wrong!"
   
 end
 
-def greeting
+def greeting = "Hi What's up?"
+name = "Eric"
 
 end
 
 def return_a_value
-
-end
+  "Chillen"
+  
+  end
 
 def last_evaluated_value
-
+'cool'
 end
 
 def pizza_party
-
+puts "pepperoni"
 end
