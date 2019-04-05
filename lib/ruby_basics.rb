47b25fb5bcfdfd47fb6838701eabbf16de7254ad
dependable-inventor-4583 <rodes88@msn.com>
2019-04-05 23:33:18 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value2 = value
 end
 
-def argue
-  
+def argue(name)
+  name
 end
 
-def greeting
-
+def greeting(name, language)
+  puts name
+  puts language
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(choice = "cheese")
+ choice
 end
