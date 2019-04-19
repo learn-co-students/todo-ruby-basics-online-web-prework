4e52e4fb6e0187af9273cca732ae524a84cfec26
Anna Lundwall <lundwalla1@gmail.com>
2019-04-19 01:52:01 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  num1=42
+  num2=7
+  42/7
 end
 
-def assign_variable(value)
-  
+def assign_variable(value="Bob")
+  puts "#{value}"
+  "#{value}"
 end
 
-def argue
-  
+def argue(phrase="I am right and you are wrong")
+  puts "#{phrase}"
+  "#{phrase}"
 end
 
-def greeting
-
+def greeting(greeting, name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert" 
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  puts topping
+  phrase=topping
 end
