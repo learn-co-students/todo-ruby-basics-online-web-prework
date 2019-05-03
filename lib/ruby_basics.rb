db526c2b865bbb91a4794b315db12bca97f1f629
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-03 10:28:02 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+36 / 6
 end
 
 def assign_variable(value)
-  
+  #{value}
+  value
 end
 
-def argue
-  
+def argue(name = "I'm right and you are wrong!"
+  #{name}
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+return_a_value = "Nice"
+return_a_value
 end
 
 def last_evaluated_value
-
+last_evaluated_value = "expert"
+last_evaluated_value
 end
 
-def pizza_party
-
+def pizza_party(pizza_party = "cheese")
+  #{pizza_party}
+  pizza_party
 end
