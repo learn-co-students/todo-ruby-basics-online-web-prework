de353b8245207e0149dd8aa2841d4be77da27134
Haley DeKeyser <hcd09@live.com>
2019-05-03 02:30:24 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(name, occupation)
+  puts "Hello, #{name}! You are a great #{occupation}."
 end
 
 def return_a_value
-
+  word = "Nice"
+  word
 end
 
 def last_evaluated_value
-
+  first_value = "expert"
+  puts first_value.reverse
+  first_value
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
