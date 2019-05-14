58adc0cb8aa5360e11b165473072109411c24845
layla dantas <laylatdantas@gmail.com>
2019-05-14 03:26:46 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1 == 42
+ num2 == 7
 end
 
 def assign_variable(value)
-  
+  puts = "Bob"
 end
 
 def argue
-  
+  puts = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def (greeting, name)
+ puts = "Hi there, "
+ puts = "Bobby!"
 end
 
 def return_a_value
-
+ puts = "Nice"
 end
 
 def last_evaluated_value
-
+puts = "Expert"
 end
 
 def pizza_party
-
+puts = "cheese"
 end
