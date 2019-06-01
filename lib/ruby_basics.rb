89eae73d3c9051eabaaea2ab900f778ebf100f30
Nick Baughman <nbaugh1@gmail.com>
2019-06-01 04:00:36 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ quotient = num1/num2
+ return quotient
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(value)
+  return value
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  itsays = "Nice"
+  return itsays
 end
 
 def last_evaluated_value
-
+  say = "expert"
+  return say
 end
 
-def pizza_party
-
+def pizza_party(yum="cheese")
+  return yum
 end
