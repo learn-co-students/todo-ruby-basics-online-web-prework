9d257e6c7c9352316edec64a206582c298cb05a9
Jessica Cervantes-Torres <cervantesjess1994@gmail.com>
2019-06-13 09:14:15 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
-def division(num1, num2)
- 
+def division(num1, num2,)
+  puts num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  puts "You are #{name}"
 end
 
-def argue
-  
+def argue(phrase)
+  puts "I'm right and you are wrong"
 end
 
-def greeting
-
+def greeting(name, language = Spanish)
+  puts "Hi, #{name}, heard you speak very good #{language}."
 end
 
-def return_a_value
-
+def return_a_value(phrase)
+  puts "nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase)
+  puts "expert"
 end
 
-def pizza_party
+def pizza_party(pizza = "cheese")
+  puts "I want #{pizza}"
+end
 
+def pizza_party("pepperoni")
+  puts "#{pepperoni}"
 end
